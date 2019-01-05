4a742139bdeb2e48b501bf9714f5461ce9719a8e
Micha <jan-micha-nietsch@t-online.de>
2019-01-05 17:50:51 +0100

    - made all functions that use gameObject synchronous to allow for removal of game objects.

--- src/typespeed/Handler.java
+++ src/typespeed/Handler.java
@@ -6,23 +6,42 @@ import java.util.ArrayList;
 public class Handler {
 	ArrayList<GameObject> gameObjects = new ArrayList<>();
 
-	public void tick(){
+	public synchronized void tick(){
 		for(GameObject game : gameObjects){
 			game.tick();
 		}
 	}
 
-	public void render(Graphics g){
+	public synchronized void render(Graphics g){
+		ArrayList<GameObject> removeObjects = new ArrayList<>();
+
 		for(GameObject game : gameObjects){
+			if(game instanceof Tile){
+				Tile t = (Tile) game;
+
+				if(t.hasReachedEnd()) {
+					removeObjects.add(t);
+					continue;
+				}
+			}
+
 			game.render(g);
+		}
+
+		for(GameObject game : removeObjects){
+			removeObject(game);
 		}
 	}
 
-	public void addObject(GameObject gameObject){
+	public synchronized void addObject(GameObject gameObject){
 		gameObjects.add(gameObject);
 	}
 
-	public void removeObject(GameObject gameObject){
-		gameObjects.add(gameObject);
+	public synchronized void removeObject(GameObject gameObject){
+		gameObjects.remove(gameObject);
+	}
+
+	public int getObjectCount(){
+		return gameObjects.size();
 	}
 }
